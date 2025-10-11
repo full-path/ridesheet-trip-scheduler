e166bbfb3efeb68aa482c45c38a65c51b5ce87e2
Kevin Chambers <kc@fullpath.io>
2025-10-10 17:15:32 -0700

    Continue to try to fix issue with disjunction for dropping trips

--- main.py
+++ main.py
@@ -335,7 +335,7 @@ def solve_http(request: Request):
 
         penalty = int(r.get("penalty", default_penalty))
         routing.solver().Add(routing.ActiveVar(pI) == routing.ActiveVar(dI))
-        routing.AddDisjunction([pI, dI], penalty)
+        routing.AddDisjunction([pI], penalty)
 
     # Search
     params = pywrapcp.DefaultRoutingSearchParameters()
